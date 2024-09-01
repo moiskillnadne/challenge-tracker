116ae91050f48c5d0c77a88d11b187e69e84e2c6
Viktor Riabkov <vriabkov@scnsoft.com>
2024-09-01 22:14:38 +0200

    animation for onDayClick

--- src/widget/Challenge/ui/CalendarDayItem.tsx
+++ src/widget/Challenge/ui/CalendarDayItem.tsx
@@ -7,11 +7,33 @@ type Props = {
 export const CalendarDayItem = ({ label, isChecked, onClick }: Props) => {
   return (
     <div
-      className={`relative flex items-center justify-center w-[50px] h-[50px] md:w-[60px] md:h-[60px] rounded-full text-white text-center font-bold text-[16px] ${isChecked ? '' : 'opacity-30'}`}
+      className={`relative flex items-center justify-center w-[50px] h-[50px] md:w-[60px] md:h-[60px] rounded-full text-white text-center font-bold text-[16px]`}
       onClick={onClick}
     >
-      {label}
-      <div className="absolute inset-0 rounded-full border-[6px] border-green"></div>
+      <span className={`${isChecked ? '' : 'opacity-30'} transition-all duration-1000`}>
+        {label}
+      </span>
+
+      <svg
+        className="absolute transition-all duration-1000"
+        width="50"
+        height="50"
+        viewBox="0 0 100 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          className="transition-all duration-1000"
+          cx="50"
+          cy="50"
+          r="45"
+          stroke="#C1FF72"
+          strokeWidth="6"
+          fill="none"
+          strokeDasharray={500}
+          strokeDashoffset={isChecked ? 0 : 500}
+        />
+      </svg>
     </div>
   );
 };
