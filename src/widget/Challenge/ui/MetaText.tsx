fc5b03349af1fd1e8fa394f8fe1b327af19031d8
Viktor Riabkov <vriabkov@scnsoft.com>
2024-09-01 22:16:36 +0200

    Make MetaText bold

--- src/widget/Challenge/ui/MetaText.tsx
+++ src/widget/Challenge/ui/MetaText.tsx
@@ -6,8 +6,8 @@ type Props = {
 export const MetaText = ({ leftLabel, rightLabel }: Props) => {
   return (
     <div className="text-[20px]">
-      <span className="text-green">{leftLabel}</span>
-      <span className="text-red">{rightLabel}</span>
+      <span className="text-green font-bold">{leftLabel}</span>
+      <span className="text-red font-bold">{rightLabel}</span>
     </div>
   );
 };
