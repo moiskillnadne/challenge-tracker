4ff97a7b70ce03f96e5fff73fe3f62d84f9ecccb
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-03 12:57:47 +0200

    fix: header - added logic based on current page

--- src/widget/Header/index.tsx
+++ src/widget/Header/index.tsx
@@ -1,31 +1,48 @@
 import { useQuery } from '@tanstack/react-query';
 import { accountService } from '../../shared/api/account.service';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { LanguageSwitcher, useCustomTranslation } from '../../feature/translation';
 
 export const Header = () => {
   const navigator = useNavigate();
   const { t } = useCustomTranslation();
+  const location = useLocation();
 
   const query = useQuery({ queryKey: ['/protected/me'], queryFn: accountService.getAccountInfo });
 
   const isAuthenticated = query.data?.data.details.user.email;
 
-  const onLoginClick = () => {
-    return navigator('/login');
+  const onNavigationClick = () => {
+    if (!isAuthenticated) {
+      return navigator('/login');
+    }
+
+    if (location.pathname === '/account') {
+      return navigator('/');
+    }
+
+    return navigator('/account');
   };
 
-  const onAccountClick = () => {
-    return navigator('/account');
+  const getButtonLabel = () => {
+    if (!isAuthenticated) {
+      return t('login');
+    }
+
+    if (location.pathname === '/account') {
+      return t('home');
+    }
+
+    return t('account');
   };
 
   return (
     <div className="relative py-[12px] px-[24px] flex justify-between">
       <button
         className="text-white font-bold text-[26px] cursor-pointer"
-        onClick={isAuthenticated ? onAccountClick : onLoginClick}
+        onClick={onNavigationClick}
       >
-        {isAuthenticated ? t('account') : t('login')}
+        {getButtonLabel()}
       </button>
 
       <div className="">
