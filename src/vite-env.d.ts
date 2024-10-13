156974a532a6b273a20b645d52dbfdb0769cdb3d
Victor Ryabkov <45964820+moiskillnadne@users.noreply.github.com>
2024-10-13 17:55:13 +0300

    Feature: App version and dev deploy pipeline (#3)
    * Added app version to the left bottom corner
    
    * CI on pull request
    
    * Deploy dev action

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_SERVER_URL: string;
   readonly VITE_SECURITY_HEADER_KEY: string;
   readonly VITE_SECURITY_HEADER_VALUE: string;
+  readonly VITE_APP_VERSION: string;
 }
 
 interface ImportMeta {
---
+++ src/widget/AppVersion/index.tsx
@@ -0,0 +1,7 @@
+export const AppVersion = () => {
+  const appVersion = import.meta.env.VITE_APP_VERSION ?? 'v0.0.0';
+
+  return (
+    <div className="absolute left-0 bottom-0 text-white/50 p-[12px] text-[10px]">{appVersion}</div>
+  );
+};
