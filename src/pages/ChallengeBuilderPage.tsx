63335626eff0e729ba84437fe152f28077f0e021
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-25 12:49:33 +0200

    Fix header

--- src/pages/ChallengeBuilderPage.tsx
+++ src/pages/ChallengeBuilderPage.tsx
@@ -1,11 +1,19 @@
+import { useCustomTranslation } from '../feature/translation';
+import { Routes } from '../shared/constants';
 import { Page } from '../shared/ui';
 import { ChallengeBuilderWidget } from '../widget/ChallengeBuilder';
 import { Header } from '../widget/Header';
 
 export const ChallengeBuilderPage = () => {
+  const { t } = useCustomTranslation();
+
   return (
     <Page>
-      <Header />
+      <Header
+        navigationButtonShown={true}
+        navigateTo={Routes.HOME}
+        labelNavigationButton={t('home')}
+      />
 
       <ChallengeBuilderWidget />
     </Page>
