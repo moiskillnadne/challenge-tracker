509910a7b3a4dbb40f73aeec82d85e2502f01ec2
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-25 11:48:32 +0200

    PWA Support

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,6 +3,19 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('../service-worker.ts')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Suspense fallback={<div>Loading..</div>}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,55 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  build: {
+    manifest: true,
+  },
+
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA({
+      registerType: 'autoUpdate', // Autoupdate Service Worker
+      includeAssets: [
+        'favicon.ico',
+        'favicon.svg',
+        'apple-touch-icon.png',
+        'favicon-48x48.png',
+        'web-app-manifest-192x192.png',
+        'web-app-manifest-512x512.png',
+      ],
+      manifest: {
+        name: 'Challenge Logger',
+        short_name: 'Challenge',
+        description: 'Build your habits',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
