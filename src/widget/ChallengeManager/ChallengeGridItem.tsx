19279bfbb36881ad3865ecc76b1ddae3dc03c433
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-25 17:30:31 +0200

    OnPress action was added

--- src/widget/ChallengeManager/ChallengeGridItem.tsx
+++ src/widget/ChallengeManager/ChallengeGridItem.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from 'react';
+
 type Props = {
   goal: string;
   isActive: boolean;
@@ -6,10 +8,59 @@ type Props = {
 };
 
 export const ChallengeGridItem = ({ goal, isActive, daysLeft, onClick }: Props) => {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [isPressed, setIsPressed] = useState(false);
+  const timerRef = useRef<number | null>(null);
+
+  const handleLongPress = () => {
+    setIsEditMode(true);
+    setIsPressed(false);
+  };
+
+  const handleTouchStart = () => {
+    // 100ms delay for press animation
+    setTimeout(() => {
+      setIsPressed(true);
+    }, 100);
+
+    timerRef.current = setTimeout(handleLongPress, 750); // 750ms for long press
+  };
+
+  const handleTouchEnd = () => {
+    setIsPressed(false);
+
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+      timerRef.current = null;
+    }
+
+    if (isEditMode) {
+      // Do some logic
+      console.log('Open edit mode');
+      return setIsEditMode(false);
+    }
+
+    if (!isEditMode) {
+      onClick();
+    }
+  };
+
+  const pressedAnimationMap = {
+    false: '',
+    true: 'scale-90',
+  };
+
+  useEffect(() => {
+    console.log(`isEditMode: ${isEditMode}`);
+  }, [isEditMode]);
+
   return (
     <div
-      className="flex flex-col items-start w-[150px] border-2 border-dotted cursor-default px-[4px] py-[4px] hover:border-solid"
-      onClick={onClick}
+      className={`${pressedAnimationMap[`${isPressed}`]} duration-700  flex flex-col items-start w-[150px] border-2 border-dotted cursor-default px-[4px] py-[4px] hover:border-solid`}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+      onMouseDown={handleTouchStart}
+      onMouseUp={handleTouchEnd}
     >
       <div className="text-white font-bold text-[18px]">{goal}</div>
       <div className="text-white text-[14px] text-white/75">
