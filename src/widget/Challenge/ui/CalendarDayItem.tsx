7f84005ebf8575dce96bdc30510c55171e190241
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-26 22:25:15 +0200

    Update the calendar`s colors

--- src/widget/Challenge/ui/CalendarDayItem.tsx
+++ src/widget/Challenge/ui/CalendarDayItem.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const CalendarDayItem = ({ label, isChecked, onClick }: Props) => {
   return (
     <div
-      className={`relative flex items-center justify-center w-[50px] h-[50px] md:w-[60px] md:h-[60px] rounded-full text-white text-center font-bold text-[16px]`}
+      className={`relative flex items-center justify-center w-[50px] h-[50px] md:w-[60px] md:h-[60px] rounded-fulltext-center font-bold text-[16px]`}
       onClick={onClick}
     >
       <span className={`${isChecked ? '' : 'opacity-30'} transition-all duration-1000`}>
