e49047cb12d6224949edd4fd8b992d59886df309
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-26 22:26:27 +0200

    Remove commented code

--- src/feature/AuthorizePasskeys/lib/useAuthenticateViaPasskeys.ts
+++ src/feature/AuthorizePasskeys/lib/useAuthenticateViaPasskeys.ts
@@ -9,39 +9,8 @@ type Props = {
 };
 
 export const useAuthenticateViaPasskeys = (props?: Props) => {
-  // const verifyLoginChallenge = useMutation({
-  //   mutationFn: authService.verifyAuthentication,
-  //   onSuccess: (data) => {
-  //     console.info('[VerifyLoginChallenge:onSuccess]', data);
-  //   },
-  //   onError: (err) => {
-  //     console.info(`[VerifyLoginChallenge:onError]: ${JSON.stringify(err)}`);
-  //   },
-  // });
-
   return useMutation({
     mutationFn: authService.authenticateKeys,
-    onSuccess: async () => {
-      // console.info('[GenerateLoginChallenge:onSuccess]', resp);
-      // const options = resp.data.options;
-      // if (options.allowCredentials && options.allowCredentials.length < 1) {
-      //   if (props?.loginIfNoCredentials) {
-      //     props.loginIfNoCredentials(variables);
-      //   }
-      //   return;
-      // }
-      // try {
-      //   console.log('Passkey options', options);
-      //   const result = await startAuthentication({ optionsJSON: options });
-      //   console.log(result);
-      //   verifyLoginChallenge.mutate({
-      //     email: variables,
-      //     challengeResponse: result,
-      //   });
-      // } catch (error: unknown) {
-      //   console.error(error);
-      // }
-    },
     onError: (err) => {
       console.error(`[GenerateLoginChallenge:onError]: ${JSON.stringify(err)}`);
 
