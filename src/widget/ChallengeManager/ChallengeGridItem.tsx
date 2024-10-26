650b312d377c80df5744044f25e19e4ad49b3e4d
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-26 11:59:28 +0200

    Disable the ability to remove challenges

--- src/widget/ChallengeManager/ChallengeGridItem.tsx
+++ src/widget/ChallengeManager/ChallengeGridItem.tsx
@@ -1,106 +1,23 @@
-import { useRef, useState } from 'react';
-import { Modal, useModalState } from '../../shared/ui';
-import { useCustomTranslation } from '../../feature/translation';
-
 type Props = {
   goal: string;
   isActive: boolean;
   daysLeft: number;
   onClick: () => void;
-
-  onChallengeRemove: () => void;
 };
 
-export const ChallengeGridItem = ({
-  goal,
-  isActive,
-  daysLeft,
-  onClick,
-  onChallengeRemove,
-}: Props) => {
-  const { t } = useCustomTranslation();
-
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [isPressed, setIsPressed] = useState(false);
-  const timerRef = useRef<number | null>(null);
-
-  const { isOpen, close, open } = useModalState();
-
-  const handleLongPress = () => {
-    setIsEditMode(false);
-    setIsPressed(false);
-    open();
-  };
-
-  const handleTouchStart = () => {
-    // 100ms delay for press animation
-    setTimeout(() => {
-      setIsPressed(true);
-      setIsEditMode(true);
-    }, 100);
-
-    timerRef.current = setTimeout(handleLongPress, 750); // 750ms for long press
-  };
-
-  const handleTouchEnd = () => {
-    setIsPressed(false);
-
-    if (timerRef.current) {
-      clearTimeout(timerRef.current);
-      timerRef.current = null;
-    }
-
-    if (isEditMode) {
-      // Do some logic
-      console.log('Open edit mode');
-      return setIsEditMode(false);
-    }
-
-    if (!isEditMode) {
-      setIsPressed(false);
-      onClick();
-    }
-  };
-
-  const pressedAnimationMap = {
-    false: '',
-    true: 'scale-90',
-  };
-
+export const ChallengeGridItem = ({ goal, isActive, daysLeft, onClick }: Props) => {
   return (
-    <>
-      <div
-        className={`${pressedAnimationMap[`${isPressed}`]} duration-700  flex flex-col items-start w-[150px] border-2 border-dotted cursor-default px-[4px] py-[4px] hover:border-solid select-none`}
-        onTouchStart={handleTouchStart}
-        onTouchEnd={handleTouchEnd}
-        onMouseDown={handleTouchStart}
-        onMouseUp={handleTouchEnd}
-      >
-        <div className="text-white font-bold text-[18px] cursor-default select-none">{goal}</div>
-        <div className="text-white text-[14px] text-white/75 cursor-default select-none">
-          {isActive ? 'in progress' : 'completed'}
-        </div>
-        <div className="text-white/75 text-[14px] cursor-default select-none">
-          Days left: <span className="font-bold">{daysLeft}</span>
-        </div>
+    <div
+      className={`duration-700  flex flex-col items-start w-[150px] border-2 border-dotted cursor-default px-[4px] py-[4px] hover:border-solid select-none`}
+      onClick={onClick}
+    >
+      <div className="text-white font-bold text-[18px] cursor-default select-none">{goal}</div>
+      <div className="text-white text-[14px] text-white/75 cursor-default select-none">
+        {isActive ? 'in progress' : 'completed'}
       </div>
-
-      <Modal
-        isOpen={isOpen}
-        onClose={close}
-        title={t('doYouWantToRemoveChallenge')}
-        leftButton={{ label: t('no'), onClick: close, color: 'black' }}
-        rightButton={{
-          label: t('yes'),
-          onClick: () => {
-            onChallengeRemove();
-            close();
-          },
-          color: 'red',
-        }}
-      >
-        <></>
-      </Modal>
-    </>
+      <div className="text-white/75 text-[14px] cursor-default select-none">
+        Days left: <span className="font-bold">{daysLeft}</span>
+      </div>
+    </div>
   );
 };
--- src/widget/ChallengeManager/ChallengeManager.tsx
+++ src/widget/ChallengeManager/ChallengeManager.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { challengeService } from '../../shared/api/challenge.service';
 import { ChallengeGridItem } from './ChallengeGridItem';
 import { mapChallengeToItem } from '../Account/lib/mappers';
@@ -6,7 +6,6 @@ import { Loader } from '../../shared/ui/Loader';
 import { CreateChallengeButton } from './CreateChallengeButton';
 import { useNavigate } from 'react-router-dom';
 import { useCustomTranslation } from '../../feature/translation';
-import { useCallback } from 'react';
 
 export const ChallengeManager = () => {
   const navigate = useNavigate();
@@ -18,19 +17,7 @@ export const ChallengeManager = () => {
     select: (data) => data.data.details,
   });
 
-  const removeChallengeMutation = useMutation({
-    mutationFn: challengeService.deleteChallenge,
-    onSuccess(_, variables) {
-      console.log(`Challenge with id: ${variables} has been removed`);
-      query.refetch();
-    },
-  });
-
   const challenges = query.data?.challenges;
-
-  const removeChallenge = useCallback((id: string) => {
-    removeChallengeMutation.mutate(id);
-  }, []);
 
   if (query.isPending) {
     return (
@@ -46,7 +33,7 @@ export const ChallengeManager = () => {
         {t('yourChallenges')}
       </div>
 
-      <div className="flex gap-[8px] flex-wrap">
+      <div className="flex flex-1 sm:justify-start justify-center gap-[14px] flex-wrap">
         {challenges &&
           challenges
             .map((item) => mapChallengeToItem(item))
@@ -59,7 +46,6 @@ export const ChallengeManager = () => {
                 onClick={() => {
                   return navigate(`/challenge/${item.id}`);
                 }}
-                onChallengeRemove={() => removeChallenge(item.id)}
               />
             ))}
 
