f6f62fbdf695869981a37b1d8b7628e7e6530314
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-11-17 13:54:10 +0700

    Added two public assets for the future use

--- src/shared/ui/Page/Page.tsx
+++ src/shared/ui/Page/Page.tsx
@@ -5,7 +5,9 @@ type Props = PropsWithChildren<unknown>
 export const Page = ({ children }: Props) => {
   return (
     <div id="page-component" className="w-screen h-dvh bg-background">
-      <div className="h-full w-full flex flex-col overflow-hidden">{children}</div>
+      <div className="h-full w-full flex flex-col overflow-hidden">
+        <div>{children}</div>
+      </div>
     </div>
   )
 }
--- src/widget/Login/ui/LoginHeader.tsx
+++ src/widget/Login/ui/LoginHeader.tsx
@@ -7,7 +7,7 @@ export const LoginHeader = () => {
   return (
     <div className="flex flex-1 justify-center items-center">
       <div className="w-full px-[16px] flex flex-col items-center">
-        <div className="w-[250px] py-[18px] px-[24px]">
+        <div className="w-[250px] py-[14px] px-[24px]">
           <img className="w-[250px]" src="/web-app-manifest-512x512.png" alt="" />
         </div>
         <Typography classNames="font-bold text-[32px] whitespace-pre-wrap text-center text-black">
